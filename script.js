6c52615504193b19a5c67d65e6b97adca7ac1f91
Mahmoud <mahmoud2m2ali@gmail.com>
2024-07-29 18:49:55 +0300

    service cards animation

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ $(document).ready(function () {
             $('header').removeClass('scrolled');
     
         }
-    })
-    
+    })   
+
     
     let imgArr = ["./imgs/slider-1-1920x879.jpg", "./imgs/slider-2-1920x879.jpg" ,"./imgs/slider-3.jpg"]
     
@@ -39,72 +39,82 @@ $(document).ready(function () {
         
 
    
-    let testimonials_list = document.querySelectorAll(".test-container")
-    let testimonials_btns = document.querySelectorAll(".test-btn")
+    // let testimonials_list = document.querySelectorAll(".test-container")
+    // let testimonials_btns = document.querySelectorAll(".test-btn")
 
-    $('#btn1').click(() => { 
+    // $('#btn1').click(() => { 
         
-        testimonials_list.forEach(item => 
-            $(item).removeClass('active-container')
-        );
+    //     testimonials_list.forEach(item => 
+    //         $(item).removeClass('active-container')
+    //     );
         
-        testimonials_btns.forEach(btn =>
-            $(btn).removeClass("test-active-btn")
-        );
+    //     testimonials_btns.forEach(btn =>
+    //         $(btn).removeClass("test-active-btn")
+    //     );
 
-        $("#btn1").addClass("test-active-btn");
+    //     $("#btn1").addClass("test-active-btn");
+    //     $(".test-container-1").addClass("active-container");
+    // });
 
+    // $('#btn2').click(() => { 
+    //     testimonials_list.forEach(item => 
+    //         $(item).removeClass('active-container')
+    //     );
 
-        $(".test-container-1").addClass("active-container");
-    });
+    //     testimonials_btns.forEach(btn =>
+    //         $(btn).removeClass("test-active-btn")
+    //     );
 
-    $('#btn2').click(() => { 
-        testimonials_list.forEach(item => 
-            $(item).removeClass('active-container')
-        );
-
-        testimonials_btns.forEach(btn =>
-            $(btn).removeClass("test-active-btn")
-        );
-
-        $("#btn2").addClass("test-active-btn");
+    //     $("#btn2").addClass("test-active-btn");
 
 
-        $(".test-container-2").addClass("active-container");
+    //     $(".test-container-2").addClass("active-container");
 
-    });
+    // });
 
-    $('#btn3').click(() => { 
+    // $('#btn3').click(() => { 
         
-        testimonials_list.forEach(item => 
-            $(item).removeClass('active-container')
-        );
-        testimonials_btns.forEach(btn =>
-            $(btn).removeClass("test-active-btn")
-        );
+    //     testimonials_list.forEach(item => 
+    //         $(item).removeClass('active-container')
+    //     );
+    //     testimonials_btns.forEach(btn =>
+    //         $(btn).removeClass("test-active-btn")
+    //     );
 
-        $("#btn3").addClass("test-active-btn");
+    //     $("#btn3").addClass("test-active-btn");
 
         
-        $(".test-container-3").addClass("active-container");
-    });
+    //     $(".test-container-3").addClass("active-container");
+    // });
 
-    $('#btn4').click(() => { 
+    // $('#btn4').click(() => { 
         
-        testimonials_list.forEach(item => 
-            $(item).removeClass('active-container')
-        );
+    //     testimonials_list.forEach(item => 
+    //         $(item).removeClass('active-container')
+    //     );
 
-        testimonials_btns.forEach(btn =>
-            $(btn).removeClass("test-active-btn")
-        );
+    //     testimonials_btns.forEach(btn =>
+    //         $(btn).removeClass("test-active-btn")
+    //     );
 
-        $("#btn4").addClass("test-active-btn");
+    //     $("#btn4").addClass("test-active-btn");
 
-        $(".test-container-4").addClass("active-container");
-    });
+    //     $(".test-container-4").addClass("active-container");
+    // });
 
 
+    function updateTestimonial(btnId, containerClass) {
+        $(".test-container").removeClass('active-container');
+        $('.test-btn').removeClass("test-active-btn");
+        $(`#${btnId}`).addClass("test-active-btn");
+        $(`.${containerClass}`).addClass("active-container");
+    }
+
+
+    $('#btn1').click(() => updateTestimonial("btn1", "test-container-1"));
+    $('#btn2').click(() => updateTestimonial("btn2", "test-container-2"));
+    $('#btn3').click(() => updateTestimonial("btn3", "test-container-3"));
+    $('#btn4').click(() => updateTestimonial("btn4", "test-container-4"));
 
 
 });
